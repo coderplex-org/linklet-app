6513ada736a5f999e12b867cfcafba02dea299d2
Vinay Puppal <me@vinaypuppal.com>
2017-03-27 17:09:23 +0530

    update to nextjs v2

--- components/Footer.js
+++ components/Footer.js
@@ -2,13 +2,6 @@ export default () => (
   <footer>
     <p>Made With ♥ By VinayPuppal</p>
     <p>
-      You can find all my other awesome projects at my site
-      {' '}
-      <a href='https://www.vinaypuppal.com' target='_blank'>
-        https://www.vinaypuppal.com
-      </a>
-    </p>
-    <p>
       ☛
       {' '}
       <a target='_blank' href='https://github.com/vinaypuppal/linklet-app'>
