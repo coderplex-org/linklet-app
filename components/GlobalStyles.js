6c827d136963892323892e1b179fbdf36507c13e
Vinay Puppal <me@vinaypuppal.com>
2017-04-14 00:38:41 +0530

    change bg color and remove ngprogress spinner

--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -13,7 +13,7 @@ export default () => (
         width: 100%;
         padding: 0;
         margin: 0;
-        background: #f4f4f4;
+        background: #fafafa;
         font-family: -apple-system,
               system-ui,
               BlinkMacSystemFont,
@@ -36,7 +36,7 @@ export default () => (
         pointer-events: none;
         }
         #nprogress .bar {
-        background: #F44336;
+        background: #c2185b;
         position: fixed;
         z-index: 1031;
         top: 0;
@@ -51,43 +51,9 @@ export default () => (
         right: 0px;
         width: 100px;
         height: 100%;
-        box-shadow: 0 0 10px #F44336, 0 0 5px #F44336;
+        box-shadow: 0 0 10px #c2185b, 0 0 5px #c2185b;
         opacity: 1.0;
         transform: rotate(3deg) translate(0px, -4px);
-        }
-        /* Remove these to get rid of the spinner */
-        #nprogress .spinner {
-        display: block;
-        position: fixed;
-        z-index: 1031;
-        top: 15px;
-        right: 15px;
-        }
-        #nprogress .spinner-icon {
-        width: 18px;
-        height: 18px;
-        box-sizing: border-box;
-        border: solid 2px transparent;
-        border-top-color: #F44336;
-        border-left-color: #F44336;
-        border-radius: 50%;
-        animation: nprogress-spinner 400ms linear infinite;
-        }
-        .nprogress-custom-parent {
-        overflow: hidden;
-        position: relative;
-        }
-        .nprogress-custom-parent #nprogress .spinner,
-        .nprogress-custom-parent #nprogress .bar {
-        position: absolute;
-        }
-        @-webkit-keyframes nprogress-spinner {
-        0%   { -webkit-transform: rotate(0deg); }
-        100% { -webkit-transform: rotate(360deg); }
-        }
-        @keyframes nprogress-spinner {
-        0%   { transform: rotate(0deg); }
-        100% { transform: rotate(360deg); }
         }
         .rc-pagination {
           font-size: 12px;
