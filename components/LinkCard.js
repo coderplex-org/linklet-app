6a4123c698e48930d526fbbc0c51ce48090b92dd
Vinay Puppal <me@vinaypuppal.com>
2017-04-14 00:37:19 +0530

    remove images and change ui

--- components/LinkCard.js
+++ components/LinkCard.js
@@ -1,18 +1,9 @@
 import distanceInWordsToNow from 'date-fns/distance_in_words_to_now'
 import format from 'date-fns/format'
-import LazyLoad from 'react-lazyload'
-import db from '../lib/db'
 import { truncateString } from '../utils'
 import Highlighter from 'react-highlight-words'
 
 export default ({ link, query: { search } }) => {
-  let imgUrl
-  // fetch http imges over proxy to avoid mixed content error
-  if (link.image && /http:.*/.test(link.image)) {
-    imgUrl = `${db.baseUrl}/proxy/${link.image}`
-  } else {
-    imgUrl = link.image
-  }
   return (
     <li key={link._id} className='list__item'>
       <a
@@ -21,20 +12,13 @@ export default ({ link, query: { search } }) => {
         href={link.url}
         target='_blank'
       >
-        <div className='item__media'>
-          {link.image
-            ? <LazyLoad height={200} offset={100}>
-              <img src={imgUrl} alt={link.title} />
-            </LazyLoad>
-            : <div className='dummy__img'>☆</div>}
-        </div>
         <div className='item__content'>
           <h3 className='title'>
             <Highlighter
               highlightClassName='highlight'
               searchWords={[search]}
               textToHighlight={
-                link.title ? truncateString(link.title, 100) : link.url
+                link.title ? truncateString(link.title, 90) : link.url
               }
             />
           </h3>
@@ -43,7 +27,7 @@ export default ({ link, query: { search } }) => {
               ? <Highlighter
                 highlightClassName='highlight'
                 searchWords={[search]}
-                textToHighlight={truncateString(link.description, 100)}
+                textToHighlight={truncateString(link.description, 140)}
                 />
               : 'No Description'}
           </p>
@@ -69,20 +53,23 @@ export default ({ link, query: { search } }) => {
         {
           `
           .list__item {
-            margin: 30px 25px;
-            background: #fff;
-            box-shadow: 0 0 5px rgba(0,0,0,0.3);
+            margin: 20px auto;
+            box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);
             width: 360px;
             transition: box-shadow .35s ease-out, transform .3s ease-out, opacity .2s ease-out;
+            display: flex;
+            border-radius: 12px;
+            background: #fff;
           }
           .list__itemLink {
             display: flex;
             flex-direction: column;
-            justify-content: center;
+            justify-content: space-between;
             cursor: pointer;
             width: 100%;
             text-decoration: none;
             color: #444;
+            padding: 10px;
           }
           .list__item:hover {
             box-shadow: 0 15px 20px rgba(0, 0, 0, 0.2);
@@ -106,19 +93,20 @@ export default ({ link, query: { search } }) => {
           }
           .item__content {
             width: 100%;
-            background: #fff;
             min-height: 127px;
           }
           .title {
+            display: flex;
+            align-items: center;
             margin: 0 0 5px 0;
             padding: 5px 10px;
-            color: #444;
             font-size: 18px;
+            min-height: 75px;
+            border-bottom: 1px solid #eee;
           }
           .desc {
             margin: 0;
             font-size: 14px;
-            color: #444;
             padding: 5px 10px;
           }
           .link {
@@ -133,6 +121,7 @@ export default ({ link, query: { search } }) => {
             display: flex;
             justify-content: space-between;
             align-items: center;
+            border-top: 1px solid #eee;
           }
           .item__footer span {
             color: #666;
@@ -153,7 +142,13 @@ export default ({ link, query: { search } }) => {
           }
           @media(max-width: 1440px) {
             .list__item {
-              margin: 30px auto;
+              margin: 20px auto;
+            }
+          }
+          @media(max-width: 720px) {
+            .list__item:hover {
+              box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12);
+              transform: translate(0, 0);
             }
           }
         `
