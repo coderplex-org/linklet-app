a94d9af268dbb42161a3fc7accc9c9722057e640
Vinay Puppal <me@vinaypuppal.com>
2017-04-17 20:48:03 +0530

    fix #6

--- components/Notification.js
+++ components/Notification.js
@@ -21,118 +21,134 @@ export default class Notification extends React.Component {
     }
     // Get `push notification` subscription
     // If `serviceWorker` is registered and ready
-    navigator.serviceWorker.ready.then(registration => {
-      registration.pushManager
-        .getSubscription()
-        .then(subscription => {
-          // If already access granted, enable push button status
-          if (subscription) {
-            this.setState({
-              checked: true
-            })
-          } else {
-            this.setState({
-              checked: false
-            })
-          }
-        })
-        .catch(error => {
-          console.error('Error occurred while enabling push ', error)
-          this.setState({
-            show: true,
-            message: 'Error occurred while enabling push '
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready.then(registration => {
+        registration.pushManager
+          .getSubscription()
+          .then(subscription => {
+            // If already access granted, enable push button status
+            if (subscription) {
+              this.setState({
+                checked: true
+              })
+            } else {
+              this.setState({
+                checked: false
+              })
+            }
           })
-        })
-    })
-  }
-  subscribePush () {
-    navigator.serviceWorker.ready.then(registration => {
-      if (!registration.pushManager) {
-        this.setState({
-          show: true,
-          message: "Sorry, Push notification isn't supported in your browser."
-        })
-        return
-      }
-      NProgress.start()
-      // To subscribe `push notification` from push manager
-      registration.pushManager
-        .subscribe({
-          userVisibleOnly: true // Always show notification when received
-        })
-        .then(subscription => {
-          console.info('Push notification subscribed.')
-          console.log(subscription)
-          return this.saveSubscriptionID(subscription)
-        })
-        .then(() => {
-          NProgress.done()
-          this.setState({
-            checked: true,
-            show: true,
-            message: 'Successfully subscribed to push notifications.'
-          })
-        })
-        .catch(error => {
-          NProgress.done()
-          this.setState({
-            checked: false,
-            show: true,
-            message: 'Push notification subscription failed'
-          })
-          console.error('Push notification subscription error: ', error)
-        })
-    })
-  }
-  unsubscribePush () {
-    navigator.serviceWorker.ready.then(registration => {
-      // Get `push subscription`
-      registration.pushManager
-        .getSubscription()
-        .then(subscription => {
-          // If no `push subscription`, then return
-          if (!subscription) {
+          .catch(error => {
+            console.error('Error occurred while enabling push ', error)
             this.setState({
               show: true,
-              message: 'Unable to unregister push notification.'
+              message: 'Error occurred while enabling push '
             })
-            return
-          }
-          NProgress.start()
-          // Unsubscribe `push notification`
-          subscription
-            .unsubscribe()
-            .then(() => {
-              console.info('Push notification unsubscribed.')
-              console.log(subscription)
-              return this.deleteSubscriptionID(subscription)
-            })
-            .then(() => {
-              NProgress.done()
-              this.setState({
-                checked: false,
-                show: true,
-                message: 'Successfully unsubscribed from push notifications.'
-              })
-            })
-            .catch(function (error) {
-              NProgress.done()
-              console.error(error)
-              this.setState({
-                show: true,
-                message: 'Failed to unsubscribe push notification.'
-              })
-            })
-        })
-        .catch(error => {
-          console.error(error)
-          console.error('Failed to unsubscribe push notification.')
+          })
+      })
+    }
+  }
+  subscribePush () {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready.then(registration => {
+        if (!registration.pushManager) {
           this.setState({
             show: true,
-            message: 'Failed to unsubscribe push notification.'
+            message: "Sorry, Push notification isn't supported in your browser."
           })
-        })
-    })
+          return
+        }
+        NProgress.start()
+        // To subscribe `push notification` from push manager
+        registration.pushManager
+          .subscribe({
+            userVisibleOnly: true // Always show notification when received
+          })
+          .then(subscription => {
+            console.info('Push notification subscribed.')
+            console.log(subscription)
+            return this.saveSubscriptionID(subscription)
+          })
+          .then(() => {
+            NProgress.done()
+            this.setState({
+              checked: true,
+              show: true,
+              message: 'Successfully subscribed to push notifications.'
+            })
+          })
+          .catch(error => {
+            NProgress.done()
+            this.setState({
+              checked: false,
+              show: true,
+              message: 'Push notification subscription failed'
+            })
+            console.error('Push notification subscription error: ', error)
+          })
+      })
+    } else {
+      this.setState({
+        show: true,
+        message: 'Some scripts are not loaded yet, try again after sometime'
+      })
+    }
+  }
+  unsubscribePush () {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready.then(registration => {
+        // Get `push subscription`
+        registration.pushManager
+          .getSubscription()
+          .then(subscription => {
+            // If no `push subscription`, then return
+            if (!subscription) {
+              this.setState({
+                show: true,
+                message: 'Unable to unregister push notification.'
+              })
+              return
+            }
+            NProgress.start()
+            // Unsubscribe `push notification`
+            subscription
+              .unsubscribe()
+              .then(() => {
+                console.info('Push notification unsubscribed.')
+                console.log(subscription)
+                return this.deleteSubscriptionID(subscription)
+              })
+              .then(() => {
+                NProgress.done()
+                this.setState({
+                  checked: false,
+                  show: true,
+                  message: 'Successfully unsubscribed from push notifications.'
+                })
+              })
+              .catch(function (error) {
+                NProgress.done()
+                console.error(error)
+                this.setState({
+                  show: true,
+                  message: 'Failed to unsubscribe push notification.'
+                })
+              })
+          })
+          .catch(error => {
+            console.error(error)
+            console.error('Failed to unsubscribe push notification.')
+            this.setState({
+              show: true,
+              message: 'Failed to unsubscribe push notification.'
+            })
+          })
+      })
+    } else {
+      this.setState({
+        show: true,
+        message: 'Some scripts are not loaded yet, try again after sometime'
+      })
+    }
   }
   saveSubscriptionID (subscription) {
     var subscriptionId = subscription.endpoint.split('gcm/send/')[1] ||
