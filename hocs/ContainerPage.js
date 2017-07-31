5ef330cb59eeafd796302e51936c7d69a835a8c2
Vinay Puppal <me@vinaypuppal.com>
2017-08-01 00:13:30 +0530

    remove unwanted script

--- hocs/ContainerPage.js
+++ hocs/ContainerPage.js
@@ -95,24 +95,6 @@ export default Page => {
         s1.charset = 'UTF-8'
         document.body.appendChild(s1)
       }
-      if (document.getElementById('hotjar-script')) {
-        window.hj = window.hj
-      } else {
-        ;(function (h, o, t, j, a, r) {
-          h.hj =
-            h.hj ||
-            function () {
-              ;(h.hj.q = h.hj.q || []).push(arguments)
-            }
-          h._hjSettings = { hjid: 551659, hjsv: 5 }
-          a = o.getElementsByTagName('head')[0]
-          r = o.createElement('script')
-          r.async = 1
-          r.id = 'hotjar-script'
-          r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv
-          a.appendChild(r)
-        })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=')
-      }
       if (!navigator.onLine) {
         const body = document.querySelector('body')
         body.style.filter = 'grayscale(1)'
